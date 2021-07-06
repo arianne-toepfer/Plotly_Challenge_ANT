d77453ac8a891c10998c0db9b8fb53c099818413
arianne-toepfer <78578889+arianne-toepfer@users.noreply.github.com>
2021-07-05 17:12:35 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -19,4 +19,13 @@ HINT 2
  Event Listener is different in this html, review id="selDataset" in index.html
  <select id="selDataset" onchange="optionChanged(this.value)"></select>
 
-*/s
+*/
+//import data
+d3.json("samples.json").then((data) => {
+   var metadata = data.metadata;
+   var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+   var result = resultArray[0];
+
+   console.log(result);
+
+});
